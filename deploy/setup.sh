4c52564a61cc90af274d14fd6721d9ff6574f8fd
amanjain487 <aman487jain@gmail.com>
2022-12-11 19:31:10 +0530

    update

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -16,31 +16,31 @@ echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
-# mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/udemy-django-api
+# mkdir -p /usr/local/apps
+git clone $PROJECT_GIT_URL /usr/local/apps/udemy-django-api
 
-mkdir -p $VIRTUALENV_BASE_PATH
+mkdir -p /usr/local/virtualenvs
 
-python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api_venv
-
-$VIRTUALENV_BASE_PATH/profiles_api_venv/bin/pip install -r $PROJECT_BASE_PATH/udemy-django-api/requirements.txt
+python3 -m venv /usr/local/virtualenvs/profiles_api_venv
+source /usr/local/virtualenvs/profiles_api_venv/bin/activate
+/usr/local/virtualenvs/profiles_api_venv/bin/pip install -r /usr/local/apps/udemy-django-api/requirements.txt
 
 # Run migrations
-cd $PROJECT_BASE_PATH/udemy-django-api/
+cd /usr/local/apps/udemy-django-api/
 pwd
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 echo "migrate done"
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/udemy-django-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp /usr/local/apps/udemy-django-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
 # Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/udemy-django-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp /usr/local/apps/udemy-django-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
 systemctl restart nginx.service
