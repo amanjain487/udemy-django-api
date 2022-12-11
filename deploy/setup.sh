092031ba612b4ca18415d08f790d0ea3f6af265f
amanjain487 <aman487jain@gmail.com>
2022-12-11 19:22:36 +0530

    update

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -16,7 +16,7 @@ echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
-mkdir -p $PROJECT_BASE_PATH
+# mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/udemy-django-api
 
 mkdir -p $VIRTUALENV_BASE_PATH
