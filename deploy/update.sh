5c994c78aea1b1a0c8250cdd6d5c5b6812787912
amanjain487 <aman487jain@gmail.com>
2022-12-11 20:18:32 +0530

    update

--- deploy/update.sh
+++ deploy/update.sh
@@ -5,11 +5,8 @@ set -e
 PROJECT_BASE_PATH='/usr/local/apps/udemy-django-api'
 
 git pull
-
-cd $PROJECT_BASE_PATH/
-python manage.py migrate
-python manage.py collectstatic --noinput
-
+$PROJECT_BASE_PATH/env/bin/python3 manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python3 manage.py collectstatic --noinput
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
