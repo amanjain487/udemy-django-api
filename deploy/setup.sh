c6ac4fa6da8556300482999b5a8d378e895a6e56
amanjain487 <aman487jain@gmail.com>
2022-12-11 19:15:07 +0530

    update

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -21,13 +21,15 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/udemy-django-api
 
 mkdir -p $VIRTUALENV_BASE_PATH
 python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api_venv
+echo "Done till here1"
 
 $VIRTUALENV_BASE_PATH/profiles_api_venv/bin/pip install -r $PROJECT_BASE_PATH/udemy-django-api/requirements.txt
+echo "Done till here2"
 
 # Run migrations
 cd $PROJECT_BASE_PATH/udemy-django-api/
 pwd
-echo "Done till here"
+echo "Done till here3"
 python manage.py migrate
 python manage.py collectstatic --noinput
 
